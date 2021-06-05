99285b88636c2b3713a039b2cef5f4c87ce52be8
unknown <shardulparlikar@gmail.com>
2021-06-05 14:44:20 +0530

    REmoved Redirect from app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter as Router , Route , Switch, Redirect} from "react-router-dom"
+import {BrowserRouter as Router , Route , Switch} from "react-router-dom"
 import NewPage from "./Component/NewPage"
 import Login from './Component/Login'
 
